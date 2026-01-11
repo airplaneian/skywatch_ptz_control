b854438f9a03dc33ccb597ccfc4c844f4d4a8d01
Ian Servin <1294186+airplaneian@users.noreply.github.com>
2026-01-10 21:28:00 -0800

    launcher bugfix

--- launch_skywatch.sh
+++ launch_skywatch.sh
@@ -1,4 +1,7 @@
-osascript -e 'tell app "Terminal"
+#!/bin/bash
+osascript -e 'tell application "Terminal"
     do script "cd \"/Users/iservin/Documents/skywatch_ptz_control\" && ./.venv/bin/python app.py"
     activate
 end tell'
+sleep 3
+open "http://localhost:5001"
